de99b43d5f8ddad9fe6ae75e030444bd540725a1
Vic92548 <victor.chanet@yahoo.com>
2021-08-22 14:10:35 +0200

    adding server lost screen

---
+++ server-lost-screen/script.js
@@ -0,0 +1,29 @@
+const ServerLostAutoRetryTime = 10000;
+const DefaultServerLostHeader = '<i class="material-icons">construction</i>' +
+'                <h1>LazyDB <span>Alpha</span></h1>' +
+'                <h5>Next-gen SaaS backend</h5>';
+
+function OpenServerLostScreen(title = "Connection lost", message = "Connection to your LazyDB server lost ", header = DefaultServerLostHeader){
+    document.body.innerHTML = '<section class="lazydb_load_error">\n' +
+        '    <div class="container">\n' +
+        '        <div class="sub_container">\n' +
+        '            <div class="header">\n' + header +
+        '            </div>\n' +
+        '            <div class="arrow-down"></div>\n' +
+        '            <div class="loading">\n' +
+        '                <h1>' + title + '</h1>\n' +
+        '                <div class="lds-ripple red"><div></div><div></div></div>\n' +
+        '                <h4 id="error_sub_title">' + message + ' (auto retry in <span>' + (ServerLostAutoRetryTime / 1000) + '</span>s)</h4>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '    </div>\n' +
+        '</section>';
+    setTimeout(function (){
+        document.location.reload();
+    },ServerLostAutoRetryTime);
+    let cpt = 10;
+    setInterval(function (){
+        cpt -= 1;
+        document.getElementById('error_sub_title').getElementsByTagName('span')[0].innerHTML=cpt
+    },ServerLostAutoRetryTime / 1000);
+}
