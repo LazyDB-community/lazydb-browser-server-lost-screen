47180ce9301bb1debad572299356a91a02ae184f
Vic92548 <victor.chanet@lazydb.com>
2021-09-13 13:01:45 +0200

    Update code_example.js

--- code_example/code_example.js
+++ code_example/code_example.js
@@ -1,15 +1,3 @@
-/*const copy_buttons = document.body.querySelectorAll("div.code div.top i.copy");
-const code_blocks = document.body.querySelectorAll("div.code > div");
-const code_lang_selector = document.body.querySelectorAll("div.code div.top > div");
-
-for (let i = 0; i < code_blocks.length; i++) {
-    copy_buttons[i].onclick = (e) => {
-        copyToClipboard(i);
-    };
-
-    highLightCode(code_blocks[i]);
-}*/
-
 function highLightCode(code_block){
 
     for (let i = 0; i < code_block.querySelectorAll("pre > code").length; i++) {
@@ -20,13 +8,11 @@ function highLightCode(code_block){
 
 }
 
-function copyToClipboard(n) {
+function copyToClipboard(el) {
     let code = "";
 
-    const code_parent = code_blocks[n].getElementsByClassName('code_lang')[0];
-
-    for (let i = 0; i < code_parent.querySelectorAll("pre > code").length; i++) {
-        const current_child = code_parent.querySelectorAll("pre > code")[i];
+    for (let i = 0; i < el.querySelectorAll("pre > code").length; i++) {
+        const current_child = el.querySelectorAll("pre > code")[i];
         code += current_child.textContent + "\n";
     }
 
@@ -34,14 +20,14 @@ function copyToClipboard(n) {
 
     navigator.clipboard.writeText(code).then(() => {
 
-        if(code_blocks[n].querySelectorAll("div.tooltip").length === 0){
+        if(el.querySelectorAll("div.tooltip").length === 0){
             const tooltip = document.createElement("div");
             tooltip.className = "tooltip";
             tooltip.textContent = "Copied to clipboard!";
-            code_blocks[n].insertBefore(tooltip, code_blocks[n].querySelector("div.top"));
+            el.insertBefore(tooltip, el.querySelector("div.top"));
 
             setTimeout(() => {
-                code_blocks[n].removeChild(tooltip);
+                el.removeChild(tooltip);
             }, 3000);
         }
     });
@@ -78,5 +64,11 @@ document.addEventListener("DOMContentLoaded", function(){
         code_example.parentElement.replaceChild(new_child, code_example);
 
         highLightCode(new_child);
+
+        code_example.querySelector("i").onclick = (e) => {
+            copyToClipboard(code_example);
+        }
+
+
     }
 });
